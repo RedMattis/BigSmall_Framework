ea5fed59caf292d78d6d922f5cc88053c4ccabeb
RedMattis <redmattis@gmail.com>
2025-09-05 22:17:43 +0200

    Improved some icons

--- 1.6/Base/Source/BigSmallFramework/Textures.cs
+++ 1.6/Base/Source/BigSmallFramework/Textures.cs
@@ -13,7 +13,7 @@ namespace BigAndSmall
 	{
 		public static Texture2D ColorPawn_Icon { get; } = ContentFinder<Texture2D>.Get("BS_UI/ColorPawn");
 		public static Texture2D Mechanical_Icon { get; } = ContentFinder<Texture2D>.Get("BS_Traits/BS_Mechanical");
-		public static Texture2D AlienIcon_Icon { get; } = ContentFinder<Texture2D>.Get("BS_Traits/Alien");
+		public static Texture2D AlienIcon_Icon { get; } = ContentFinder<Texture2D>.Get("BS_UI/Race");
 		public static Texture2D BrightnessTexture { get; } = ContentFinder<Texture2D>.Get("BS_UI/BrightnessGradient", true);
 		public static Texture2D SliderHandle { get; } = ContentFinder<Texture2D>.Get("UI/Buttons/SliderHandle");
 	}
